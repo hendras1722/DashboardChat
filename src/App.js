1eff370fd23dbaf58ac06ee7f64722b473f52d30
hendras1722 <57746184+hendras1722@users.noreply.github.com>
2020-04-14 03:08:55 +0700

    added feature sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+// import store from './Component/redux/store';
 import './App.css';
+import Home from './Component/Home/Homescreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider store={store}>
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
+    // </Provider>
   );
 }
 
---
+++ src/Component/Layout/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Navbar extends Component {
+    render() {
+        return (
+            <>
+                <p>hello</p>
+            </>
+        )
+    }
+}
+
+export default Navbar;
